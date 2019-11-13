e8dace005f9ad532debd507771bbaba9293dac5c
Alexandre94H <alexandre@bithub.codes>
2019-11-13 19:30:07 +0100

    bug updated

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ahallain <ahallain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/12 20:13:27 by ahallain          #+#    #+#             */
-/*   Updated: 2019/11/13 16:47:24 by ahallain         ###   ########.fr       */
+/*   Updated: 2019/11/13 18:35:47 by ahallain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,13 @@ int		get_next_line(int fd, char **line)
 	char		*buffer;
 	int			ret;
 	int			read_value;
-	int			size;
 
-	size = BUFFER_SIZE < 100000 ? BUFFER_SIZE : 128;
 	if ((!memory && !(memory = malloc(1))
 		&& (*memory = 0))
-		|| !(buffer = malloc(size + 1)))
+		|| !(buffer = malloc(BUFFER_SIZE + 1)))
 		return (-1);
 	while (!ft_strcchr(memory, '\n')
-		&& (read_value = read(fd, buffer, size)) > 0)
+		&& (read_value = read(fd, buffer, BUFFER_SIZE)) > 0)
 	{
 		buffer[read_value] = 0;
 		ft_stradd(&memory, buffer);
