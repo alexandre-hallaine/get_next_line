cdbf53d183cade79a92d3fdfc2c79f90165b2335
Alexandre Hallaine <ahallain@student.42.fr>
2021-02-17 16:09:07 +0100

    remove: prototype

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: ahallain <ahallain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/08 21:50:37 by ahallain          #+#    #+#             */
-/*   Updated: 2021/02/08 22:59:59 by ahallain         ###   ########.fr       */
+/*   Updated: 2021/02/17 16:08:26 by ahallain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@
 # include <stdbool.h>
 # include <stddef.h>
 
-int		get_next_line(int fd, char **line);
 bool	ft_includes(char *str, char c);
 void	ft_addstr(char *from, char **to);
 char	*ft_getstr(char *from, char c);
