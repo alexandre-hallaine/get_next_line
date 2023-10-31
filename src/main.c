c0b9011dd6a85f7f6a1d4103e67a6a451409a56f
Alexandre Hallaine <hallainealexandre@gmail.com>
2023-10-31 13:03:15 -0400

    Time test
    Added a time test to the main file

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <time.h>
 
 char *get_next_line(int fd);
 
@@ -15,20 +16,25 @@ int main(int argc, char **argv)
 {
 	if (argc > 2)
 		error("usage: ./a.out <file>");
-
 	int fd = argc == 1 ? 0 : open(argv[1], O_RDONLY);
 	if (fd == -1)
 		error("an error occured with open");
 
-	size_t index = 0;
+	clock_t start, end;
 	char *str;
-	while ((str = get_next_line(fd)))
-	{
-		printf("%zu: %s", ++index, str);
-		free(str);
-	}
+	size_t index = 0;
 	(void)index;
 
+	start = clock();
+	while ((str = get_next_line(fd)))
+	{
+		// printf("%zu: %s", ++index, str);
+		free(str);
+	}
+	end = clock();
 	close(fd);
+
+	double cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+	printf("get_next_line took %f seconds to execute.\n", cpu_time_used);
 	return 0;
 }
